a8567bc8943c9cd0982873d8761c0a81a3c0275e
KyubinHwang <choco1drink@gmail.com>
2022-05-04 21:51:44 +0900

    fetch code 추가 질문 db 가져올 준비 완료

--- graduation-react/src/screens/test_screen.js
+++ graduation-react/src/screens/test_screen.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Modal } from 'react-bootstrap';
 import style from './test.module.css';
 
+const url = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/681b25e4-6f4b-40a8-a6e6-293ec5390827/sample.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220504%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220504T114513Z&X-Amz-Expires=86400&X-Amz-Signature=2ffa4c3b48ba53045986654b989cf10567226ad0b6c6f6d2e44506adf5ab116f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22sample.json%22&x-id=GetObject';
+
 function RetryButton(props) {
   return(
     <Modal
@@ -54,6 +56,7 @@ function Header(){
   const [retryShow, setRetryShow] = useState(false);
   const [thinking, setThinking] = useState(true);
   const [endShow, setEndShow] = useState(false);
+  const [content, setContent] = useState([]);
 
   const onClick = () => {
     if(thinking){
@@ -68,10 +71,10 @@ function Header(){
           setQuestion(question + 1);
           setSecond(60);
           setThinking(true);
+          setSecondCheck(!secondCheck);
         }
         else{
           setEndShow(true);
-          setThinking(true);
         }
       }
     }
@@ -81,6 +84,16 @@ function Header(){
     const timer = setInterval(() => {
       setSecond(second - 1)
     }, 1000);
+    // endpoint  /question?no=
+      // no=숫자 이 때 숫자는 params로 질문 번호 될 예정
+      //url 뒤에 url + question으로 요청시킬거
+    fetch(url).then(
+      response=>{
+        return response.json();
+      }
+    ).then(data=>{
+      setContent(data);
+    });
     if(question === 5 && second === -1 && !thinking){
       setEndShow(true);
       setSecond(0);
@@ -100,12 +113,12 @@ function Header(){
       }
     }
     return () => clearInterval(timer);
-  })
+  },[question, second, thinking, secondCheck])
 
   return(
     <>
       <div className={style.article}>
-        <h2 className={style.question}>Q{question}. 질문 내용 </h2>
+        <h2 className={style.question}>Q{question}. {content.content} </h2>
         <div className={style.box2}>
         <h4>{second} {thinking ? "(생각시간)" : "(답변시간)"} </h4>
           <Button variant="outline-primary" onClick={onClick} className={style.button}>다음 질문</Button>
