19fd3efee1ff56dc83553d10fd4aa3519273b8bc
KyubinHwang <choco1drink@gmail.com>
2022-07-07 18:35:26 +0900

    결과 분석 페이지 레이아웃 잡아두기

--- frontend/src/screens/report_screen.js
+++ frontend/src/screens/report_screen.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import style from './report.module.css';
 
 const url = 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d46e4d9c-6d6d-438d-86b2-ef19867ff3be/resultSample.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220517%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220517T115040Z&X-Amz-Expires=86400&X-Amz-Signature=4c368877a0f59a4a029f49998606e6a3fc6c13fea938cae130619e148ab62b39&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22resultSample.json%22&x-id=GetObject'
@@ -43,26 +45,53 @@ function Score(){
             <h3>총 평가</h3>
             <h3>S 등급</h3>
           </div>
-          <Button variant="outline-primary" className={style.button}>더 자세한 결과 보기</Button>
+          <Button className={style.button}>더 자세한 결과 보기</Button>
         </div>
       </div>
   );
 }
+
+function JustifiedExample() {
+  return (
+    <div >
+      <Tabs
+        defaultActiveKey="evaluate1"
+        id="justify-tab-example"
+        justify
+        className={style.form}
+      >
+        <Tab eventKey="evaluate1" title="종합 총평">
+          <div className={style.Evaluate}>
+            <div className={style.contentEvaluate}>
+              <label>총 평가</label>
+            </div>
+          </div>
+        </Tab>
+        <Tab eventKey="evaluate2" title="세부 분석">
+          <div className={style.Evaluate}>
+            <div className={style.contentEvaluate}>
+              <label>세부 항목</label>
+            </div>
+          </div>
+        </Tab>
+      </Tabs>
+    </div>
+  );
+}
+
 
 class Report extends Component {
 
   render() {
     return (
         <div className={style.box}>
-          <div>
-            <h4> 규빈 님의 면접 분석 결과입니다. </h4>
-            <div>
-              <Score/>
-              <Link to="/">
-                <Button variant="outline-primary" className={style.button}>다시하기</Button>
-              </Link>
-            </div>
+          <img src = "images/currentPage4.png" className={style.currentPage} alt="profile" />
+          <div className={style.reportForm}>
+            <JustifiedExample/>
           </div>
+          <Link to="/">
+            <Button className={style.button}>처음 화면으로 돌아가기</Button>
+          </Link>
         </div>
     );
   }
