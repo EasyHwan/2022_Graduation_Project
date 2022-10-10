ed0ff88ba3f220cfebc8211262faec8b9c875dd8
junki <wnsrlsladlek@naver.com>
2022-10-10 21:55:45 +0900

    feat(flask): s3 업로드 api 추가

--- my_flask.py
+++ my_flask.py
@@ -6,6 +6,11 @@ from dotenv import load_dotenv
 import os
 from flask_cors import CORS
 
+import sys, os, base64, datetime, hashlib, hmac
+import requests # pip install requests
+
+import boto3
+
 app = Flask(__name__)
 CORS(app)
 
@@ -60,5 +65,92 @@ def question():
         return "you have to include question_no (2 <= no <= 5)"
 
 
+def sign(key, msg):
+    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+
+
+def getSignatureKey(key, dateStamp, regionName, serviceName):
+    kDate = sign(('AWS4' + key).encode('utf-8'), dateStamp)
+    kRegion = sign(kDate, regionName)
+    kService = sign(kRegion, serviceName)
+    kSigning = sign(kService, 'aws4_request')
+    return kSigning
+
+
+@app.route('/s3-signature')
+def s3_signature():
+    method = 'GET'
+    service = 'ec2'
+    host = 'ec2.amazonaws.com'
+    region = 'ap-northeast-2'
+    endpoint = 'https://ec2.amazonaws.com'
+
+    access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+    secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    if access_key is None or secret_key is None:
+        return 'No access key is available.'
+
+    t = datetime.datetime.utcnow()
+    amzdate = t.strftime('%Y%m%dT%H%M%SZ')
+    datestamp = t.strftime('%Y%m%d') # Date w/o time, used in credential scope
+
+    canonical_uri = '/'
+
+    canonical_headers = 'host:' + host + '\n' + 'x-amz-date:' + amzdate + '\n'
+
+    signed_headers = 'host;x-amz-date'
+
+    payload_hash = hashlib.sha256(('').encode('utf-8')).hexdigest()
+
+    canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
+
+    algorithm = 'AWS4-HMAC-SHA256'
+    credential_scope = datestamp + '/' + region + '/' + service + '/' + 'aws4_request'
+    string_to_sign = algorithm + '\n' + amzdate + '\n' + credential_scope + '\n' + hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
+
+    signing_key = getSignatureKey(secret_key, datestamp, region, service)
+
+    signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
+
+    authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' + 'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
+
+    headers = {'x-amz-date':amzdate, 'Authorization':authorization_header}
+
+    # ************* SEND THE REQUEST *************
+    request_url = endpoint + '?' + canonical_querystring
+
+    print('\nBEGIN REQUEST++++++++++++++++++++++++++++++++++++')
+    print('Request URL = ' + request_url)
+    r = requests.get(request_url, headers=headers)
+
+    print('\nRESPONSE++++++++++++++++++++++++++++++++++++')
+    print('Response code: %d\n' % r.status_code)
+    print(r.text)
+
+
+@app.route('/s3-upload', methods=['POST'])
+def s3_upload():
+    try:
+        file_name = request.form['file_name']
+        data = request.form['data']
+        bucket = os.environ.get('BUCKET_NAME')
+        s3 = boto3.client(
+            's3',
+            region_name='ap-northeast-2',
+            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+        )
+        content="requirement.txt"
+        #s3.Object('s3-video-example', 'exxx.txt').put(Body=content)
+        s3.put_object(
+            Bucket=bucket,
+            Body=data,
+            Key=file_name,
+            ContentType='video/mp4')
+        return "success"
+    except Exception as e:
+        print('예외가 발생했습니다.', e)
+        return str(e)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
