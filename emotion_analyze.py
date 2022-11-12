21a0488caa8df7adaeb7d254c8eabf764946e08a
junki <wnsrlsladlek@naver.com>
2022-11-12 23:09:35 +0900

    feat(emotion): emotion flask 추가

--- emotion_analyze.py
+++ emotion_analyze.py
@@ -4,27 +4,31 @@ import os
 import sys
 import pandas as pd
 
-location_vedeofile = "himan.mp4"
+def emotion(emotion_data):
+    location_vedeofile = emotion_data
 
-face_detector = FER()
-input_video = Video(location_vedeofile)
+    face_detector = FER()
+    input_video = Video(location_vedeofile)
 
-processing_data = input_video.analyze(face_detector, display=False, frequency=6)
-vid_df = input_video.to_pandas(processing_data)
-vid_df = input_video.get_first_face(vid_df)
-vid_df = input_video.get_emotions(vid_df)
+    processing_data = input_video.analyze(face_detector, display=False, frequency=6)
+    vid_df = input_video.to_pandas(processing_data)
+    vid_df = input_video.get_first_face(vid_df)
+    vid_df = input_video.get_emotions(vid_df)
 
-pltfig = vid_df.plot(figsize=(20, 8), fontsize=16).get_figure()
-angry = sum(vid_df.angry)
-disgust = sum(vid_df.disgust)
-fear = sum(vid_df.fear)
-happy = sum(vid_df.happy)
-sad = sum(vid_df.sad)
-surprise = sum(vid_df.surprise)
-neutral = sum(vid_df.neutral)
+    pltfig = vid_df.plot(figsize=(20, 8), fontsize=16).get_figure()
+    angry = sum(vid_df.angry)
+    disgust = sum(vid_df.disgust)
+    fear = sum(vid_df.fear)
+    happy = sum(vid_df.happy)
+    sad = sum(vid_df.sad)
+    surprise = sum(vid_df.surprise)
+    neutral = sum(vid_df.neutral)
 
-emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutal']
-emotions_values = [angry, disgust, fear, happy, sad, surprise, neutral]
-score_comparisons = pd.DataFrame(emotions, columns=['Human Emotions'])
-score_comparisons['Emotion Value from the Video'] = emotions_values
-print(score_comparisons)
+    emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutal']
+
+    return (angry, disgust, fear, happy, sad, surprise, neutral)
+
+    emotions_values = [angry, disgust, fear, happy, sad, surprise, neutral]
+    score_comparisons = pd.DataFrame(emotions, columns=['Human Emotions'])
+    score_comparisons['Emotion Value from the Video'] = emotions_values
+    print(score_comparisons)
---
+++ emotion_flask.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask import request, Response
+import pymysql
+import json
+from dotenv import load_dotenv
+import os
+from flask_cors import CORS, cross_origin
+import bcrypt
+from collections import defaultdict
+
+import sys, os, base64, datetime, hashlib, hmac
+import requests # pip install requests
+from emotion_analyze import *
+
+
+app = Flask(__name__)
+#CORS(app, supports_credentials=True)
+CORS(app, resources={r'*': {'origins': ['https://letmeinterview.vercel.app', 'http://localhost:3000']}}, supports_credentials=True)
+
+
+@app.route('/')
+def index():
+    my_res = {"res": "hi"}
+    return my_res
+
+
+@app.route('/emotion', methods=['POST'])
+def emotion_post():
+    mov_data = request.files['data']
+    name = request.args.get('name')
+    angry, disgust, fear, happy, sad, surprise, neutral = emotion(data)
+    ret = {
+        "angry": angry,
+        "disgust": disgust,
+        "fear": fear,
+        "happy": happy,
+        "sad": sad,
+        "surprise": surprise,
+        "neutral": neutral,
+    }
+    return ret
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
